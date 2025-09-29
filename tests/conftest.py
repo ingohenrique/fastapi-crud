d8c98164cc5b55d7d32f07842e09fe438b0b1960
ingohenrique <ingohenrique@gmail.com>
2025-09-28 23:32:54 -0300

    fix: update import statement for SQLModel in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ from fastapi.testclient import TestClient
 from sqlmodel import create_engine, Session
 from src.main import app
 from src.infra.database.session import get_session
-from src.infra.database.models import *
+from src.infra.database.models import SQLModel
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
 engine = create_engine(
